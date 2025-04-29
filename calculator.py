57e3fb97296de638f53e34324ae1bb0af7fab42e
lmocs <loganmoreno.cs@gmail.com>
2025-04-29 15:38:42 -0700

    Create three calculations.

---
+++ calculator.py
@@ -0,0 +1,27 @@
+import math
+
+def calculateTip():
+    total = int(input("enter a total, before tax: "))
+    tip = int(input("enter the percent you want to tip: "))
+    send = total * (tip / 100)
+    total += send
+    return "{:.2f}".format(total)
+
+def calculateCompoundInterest():
+    principal = float(input("enter principal: "))
+    interest_rate = float(input("enter interest rate: "))
+    compound_rate = float(input("enter compound rate: "))
+    time = float(input("enter time in years: "))
+    total = principal * math.pow((1 + interest_rate / compound_rate), compound_rate * time)
+    return "{:.2f}".format(total)
+
+def calculateInternshipPay():
+    weeks = int(input("enter number of weeks: "))
+    hours = int(input("enter number of hours per week: "))
+    rate = float(input("enter pay rate per hour: "))
+    total = rate * hours * weeks
+    return "{:.2f}".format(total)
+
+# print(calculateTip())
+# print(calculateInternshipPay())
+# print(calculateCompoundInterest())
